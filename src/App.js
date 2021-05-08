3fde1e50e1a2bd66c691e0d9fd8fd0a59d55e3c4
kuuchan67 <83497138+kuuchan67@users.noreply.github.com>
2021-05-08 15:10:07 +0900

    Route追加

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
 import { Home } from "./home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
@@ -5,10 +7,25 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
